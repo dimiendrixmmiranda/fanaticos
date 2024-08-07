c5953db8750951dc444f4713f80b2a2f4c72d9a2
Dimi Martins <dimiendrixmmiranda@gmail.com>
2024-08-07 19:55:21 -0300

    update: implementação inicial do carrossel principal

---
+++ src/components/Carrossel.tsx
@@ -0,0 +1,5 @@
+export default function Carrossel() {
+    return (
+        <div style={{height: "70vh"}} className="w-screen bg-gray-700"></div>
+    )
+}
--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -1,9 +1,11 @@
+import Carrossel from "./Carrossel";
 import Menu from "./Menu";
 
 export default function Conteudo(){
     return (
         <main>
             <Menu/>
+            <Carrossel/>
         </main>
     )
 }
