ad81c69b1e85e7323e9764708844da0b5ebdae4f
dimiendrixmmiranda <dimiendrixmmiranda@gmail.com>
2025-09-28 14:20:26 -0300

    update: troca no componente de banners

--- src/components/banners/Banners.tsx
+++ src/components/banners/Banners.tsx
@@ -1,9 +1,28 @@
-export default function Banners(){
+"use client";
+
+import React from "react";
+import { WobbleCard } from "../ui/wobble-card";
+
+export default function Banners() {
     return (
-        <div className="p-4 flex flex-col items-center gap-4 md:grid md:grid-cols-2 lg:gap-4 lg:h-[700px]">
-            <div className="w-[280px] h-[280px] bg-zinc-400 p-2 md:w-full lg:h-full">Banner 1</div>
-            <div className="w-[280px] h-[280px] bg-zinc-400 p-2 md:w-full lg:h-full">Banner 2</div>
-            <div className="w-[280px] h-[280px] bg-zinc-400 p-2 col-start-1 col-end-3 md:w-full lg:h-full">Banner 3</div>
+        <div className="p-4 h-[1000px] grid grid-cols-1 lg:grid-cols-4 gap-4 max-w-7xl mx-auto w-full lg:h-[700px]">
+            <WobbleCard
+                containerClassName="lg:col-start-1 lg:col-end-3"
+            >
+                <div className="flex justify-center items-center h-full w-full">
+                    <h2>Banner 1</h2>
+                </div>
+            </WobbleCard>
+            <WobbleCard containerClassName="lg:col-start-3 lg:col-end-5">
+                <div className="flex justify-center items-center h-full w-full">
+                    <h2>Banner 2</h2>
+                </div>
+            </WobbleCard>
+            <WobbleCard containerClassName="col-span-1 lg:col-span-5 bg-blue-900">
+                <div className="flex justify-center items-center h-full w-full">
+                    <h2>Banner 3</h2>
+                </div>
+            </WobbleCard>
         </div>
     )
 }
